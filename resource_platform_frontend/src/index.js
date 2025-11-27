452205ae842a741dfa47c9d9b7bc0468e5a98c6e
Kavia AI <production@kavia.ai>
2025-11-27 09:12:58 +0000

    Checkpoint 2 - cg551837ef

--- resource_platform_frontend/src/index.js
+++ resource_platform_frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/theme.css';
+import './styles/layout.css';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <a href="#main-content" className="visually-hidden">Skip to content</a>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
